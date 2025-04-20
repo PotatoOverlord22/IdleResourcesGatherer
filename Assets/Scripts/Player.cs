60ac99e7a8db62a70024b955533c25eb071d7e8c
PotatoOverlord22 <tiron.raul22@gmail.com>
2025-04-20 13:22:48 +0300

    fix camera and player movement, rename things, color cool glasses

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float rotateSpeed = 10f;
     [SerializeField] private GameInput gameInput;
     [SerializeField] private CinemachineCamera followCamera;
+    [SerializeField] private Transform playerModel;
 
     private Boolean isWalking;
 
@@ -31,13 +33,12 @@ public class Player : MonoBehaviour
 
         transform.position += moveDirection * moveSpeed * Time.deltaTime;
 
-        //isWalking = moveDirection != Vector3.zero;
+        isWalking = moveDirection != Vector3.zero;
 
-        //if (isWalking)
-        //{
-        //    float rotateSpeed = 10f;
-        //    transform.forward = Vector3.Slerp(transform.forward, moveDirection, rotateSpeed * Time.deltaTime);
-        //}
+        if (isWalking)
+        {
+            playerModel.forward = Vector3.Slerp(playerModel.forward, moveDirection, rotateSpeed * Time.deltaTime);
+        }
     }
 
     public bool IsWalking()
